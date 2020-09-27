03b9ec8635a5a908a39a8cfba8b177a8ccab3457
Nancy Manzo <68658424+nancymanzo@users.noreply.github.com>
2020-09-26 20:34:05 -0500

    Update Sesion6.R

--- Sesion6.R
+++ Sesion6.R
@@ -48,6 +48,98 @@ ile %>%
 
 ggplotly(g1, tooltip = c("Año", "Total"))
 
+##Otra forma de replicar el gráfico anterior:
+ile %>%
+ filter(!is.na(año)) %>% #Filtro para quitar mi valor NA
+ ggplot(aes(x = año, fill = año)) +
+ geom_bar() + #Tipo de gráfico
+ scale_fill_brewer(palette = "Set2") + #Palette para colorear y representar mi gráfico
+ labs(title = "Número total de mujeres que accedieron al ILE en la CDMX.", 
+      caption = "Fuente: Datos abiertos de la CDMX https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/export/.", fill = "AÑO",
+      x = "", 
+      y = "") +
+ ggthemes::theme_tufte() ->g2 #Se necesita ggthemes.
+
+  ggplotly(g2, tooltio= c("año"))
+
+
+
+#Más gráficos
+ile %>%
+ filter(!is.na(año)) %>%
+ filter(!(entidad %in% c("CIUDAD DE MEXICO", "ESTADO DE MEXICO")) & !is.na(entidad)) %>%
+ ggplot(aes(x = entidad, fill = entidad, weight = año)) +
+ geom_bar() +
+ scale_fill_viridis_d(option = "viridis") +
+ labs(x = " ", 
+      y = " ", 
+      title = "Total de ILE por entidades realizados en CDMX.", 
+      subtitle = "Se omitió las entidades de CDMX y EDOMEX", 
+      caption = "Fuente: Datos abiertos de la CDMX https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/export/.", fill = "ENTIDAD") +
+ theme_void() +
+ facet_wrap(vars(año)) ->g3
+
+  ggplotly(g3)
+
+
+###
+ile %>%
+ filter(año >= 2020L & año <= 2020L & !is.na(año)) %>%
+ ggplot(aes(x = edad, 
+            y = nhijos, 
+            colour = edad, 
+            size = nhijos) +
+ geom_point() +
+ scale_color_distiller(palette = "PuRd") +
+ labs(x = "Edad de las mujeres", 
+      y = "Número de hijxs", 
+      title = "Edad de las mujeres que accedieron al ILE y número de hijxs en la CDMX, 2020.", 
+      subtitle = "El rango de edad incluye desde los 11 años.", 
+      caption = "Fuente: Datos abiertos de la CDMX https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/export/.", 
+      color = "Edad", 
+      size = "Número de hijxs (0-6)") +
+ theme_minimal() ->g4
+
+ ggplotly(g4)
+
+        
+####
+ile %>%
+ filter(año >= 2016L & año <= 2019L | is.na(año)) %>%
+ filter(!is.na(ocupacion)) %>%
+ filter(!(entidad %in% c("CIUDAD DE MEXICO", "ESTADO DE MEXICO")) & !is.na(entidad)) %>%
+ ggplot(aes(x = ocupacion, fill = ocupacion)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Dark2") +
+  labs(x = " ", 
+       y = " ", 
+       title = "Ocupación de las mujeres que accedieron al ILE en CDMX", 
+       subtitle = "2017-2019", 
+       caption = "Fuente: Datos abiertos de la CDMX https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/export/.", fill = "Ocupación") +
+  theme_void() +
+  facet_wrap(vars(año)) ->g5
+        
+ ggplotly(g5)
+
+        
+###
+ile %>%
+  filter(año >= 2016L & año <= 2016L | is.na(año)) %>%
+  ggplot(aes(x = npartos, 
+             y = naborto, 
+             colour = naborto, 
+             size = naborto)) +
+  geom_point() +
+  scale_color_viridis_c(option = "viridis") +
+  labs(x = "Número de partos", 
+       y = "Número de abortos", 
+       title = "Relación de las mujeres que accedieron al ILE en CDMX con el número de partos.", 
+       subtitle = "Año 2016", 
+       caption = "Fuente: Datos abiertos de la CDMX https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/export/") +
+  theme_bw()
+
+
+
 
 
 #Características de las usuarias
@@ -61,7 +153,7 @@ t1 <- ile_edad %>%
             MAD = mad(edad),
             IQR = IQR(edad))
 
-
+        #Histograma
         ile %>% 
           filter(año==2020) %>% 
           ggplot(aes(x=edad, fill="salmon3"))+
